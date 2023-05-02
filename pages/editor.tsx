98812e3eb2f1f5a8991b6fd1b0153c9f13d92f8a
Sadullo <sadullo@gmail.com>
2023-05-02 21:13:20 +0500

    refactor: apply eslint and prettier

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/Header";
-import Head from "next/head";
+import Header from '@/components/Header';
+import Head from 'next/head';
 
 export default function EditorPage() {
   return (
