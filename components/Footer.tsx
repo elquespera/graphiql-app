fc3471f3df539ec9869ba6c4b52b2d0d47322ccd
Tomas Zagorskis <tomzagos@gmail.com>
2023-05-02 19:51:19 +0300

    feat: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<div className='fixed left-0 right-0 bottom-0 h-12 w-full p-2 flex gap-2 items-center bg-slate-800 text-slate-100'>
+			Footer
+		</div>
+	);
+}
