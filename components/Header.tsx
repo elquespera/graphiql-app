5625a013ee1bdc9788c11f464ff04bc79d778750
Tomas Zagorskis <tomzagos@gmail.com>
2023-05-03 17:34:26 +0300

    feat: add navigation into header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,35 @@
+import Link from 'next/link';
+import router from 'next/router';
+
 export default function Header() {
   return (
-    <header className="fixed left-0 right-0 top-0 h-12 w-full p-2 flex gap-2 items-center bg-slate-800 text-slate-100">
+    <header className="fixed left-0 right-0 top-0 h-12 w-full px-8 flex gap-2 justify-between items-center bg-slate-800 text-slate-100">
       <h1>GraphiQL</h1>
+      <div className="flex gap-8">
+        <Link href={'/'} className={router.pathname == '/' ? 'underline underline-offset-8' : ''}>
+          Home
+        </Link>
+        <Link
+          href={'/editor'}
+          className={router.pathname == '/editor' ? 'underline underline-offset-8' : ''}
+        >
+          IDE
+        </Link>
+      </div>
+      <div className="flex gap-4">
+        <Link
+          href={'/sign-in'}
+          className={router.pathname == '/sign-in' ? 'underline underline-offset-8' : ''}
+        >
+          Sign-in
+        </Link>
+        <Link
+          href={'/sign-up'}
+          className={router.pathname == '/sign-up' ? 'underline underline-offset-8' : ''}
+        >
+          Sign-up
+        </Link>
+      </div>
     </header>
   );
 }
