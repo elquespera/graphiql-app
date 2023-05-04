39c4e6089cb7104f1b7b0233f849d44f7b78163c
Tomas Zagorskis <tomzagos@gmail.com>
2023-05-04 14:35:35 +0300

    feat: link to sign-in or logout depends on isAuth

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,18 @@
+import { useAppSelector } from '@/redux/hooks';
+import { selectAuth } from '@/redux/auth';
+import { logOut } from '@/auth/firebaseAuth';
 import NavLink from './NavLink';
+import { useRouter } from 'next/router';
 
 export default function Header() {
+  const { isAuth } = useAppSelector(selectAuth);
+  const router = useRouter();
+
+  const logOutUser = async () => {
+    await logOut();
+    router.replace('/');
+  };
+
   return (
     <header className="fixed left-0 right-0 top-0 h-12 w-full px-8 flex gap-2 justify-between items-center bg-slate-800 text-slate-100">
       <h1>GraphiQL</h1>
@@ -8,10 +20,17 @@ export default function Header() {
         <NavLink label="Home" pathName="/" />
         <NavLink label="IDE" pathName="/editor" />
       </div>
-      <div className="flex gap-4">
-        <NavLink label="Sign-in" pathName="/sign-in" />
-        <NavLink label="Sign-up" pathName="/sign-up" />
-      </div>
+      {isAuth && (
+        <button onClick={logOutUser} className="basis-[7.6rem]">
+          Log out
+        </button>
+      )}
+      {!isAuth && (
+        <div className="flex gap-4">
+          <NavLink label="Sign-in" pathName="/sign-in" />
+          <NavLink label="Sign-up" pathName="/sign-up" />
+        </div>
+      )}
     </header>
   );
 }
