0b00d0bb3d96054b316f103278e5dedf4ecbb67d
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-05-04 17:20:53 +0200

    fix: styles

--- components/Response.tsx
+++ components/Response.tsx
@@ -6,28 +6,28 @@ export default function Response() {
   const query = useAppSelector(selectGraphQlQuery);
   const [queryGraphQL, { data, error, isError, isLoading }] = useGraphQLMutation();
 
-  const errorMsg = error && 'data' in error ? JSON.stringify(error.data) : '';
+  const errorMsg = error && 'data' in error ? JSON.stringify(error.data, null, ' ') : '';
 
   const handleSubmit = () => {
     queryGraphQL(query);
   };
 
   return (
-    <div className="flex-1 p-4 flex flex-col overflow-hidden">
+    <div className="flex-1 p-4 flex flex-col">
       <h1>Response</h1>
       <div className="flex flex-col">
         <button onClick={handleSubmit} className="bg-slate-100 text-slate-800 p-2">
           Submit
         </button>
-        <div className="mt-2 break-all overflow-y-scroll">
+        <pre className="mt-2 break-all whitespace-pre-wrap overflow-y-auto h-[calc(100vh-12rem)]">
           {isLoading
             ? 'Loading...'
             : isError
             ? errorMsg
             : data
-            ? JSON.stringify(data)
+            ? JSON.stringify(data, null, ' ')
             : 'Click Submit to fetch data'}
-        </div>
+        </pre>
       </div>
     </div>
   );
