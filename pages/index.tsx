bd1efc4648f359e07fbffe5dc590103564bcd83c
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-05-05 12:18:24 +0200

    feat: add some translations for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,19 +10,16 @@ export default function Home() {
 
   const { isAuth, userEmail } = useAppSelector(selectAuth);
 
-  const signUpTest = async () => {
-    const res = await signUp('test@email.com', '12345678');
-    console.log(res);
+  const signUpTest = () => {
+    signUp('test@email.com', '12345678');
   };
 
-  const signInTest = async () => {
-    const res = await signIn('test@email.com', '12345678');
-    console.log(res);
+  const signInTest = () => {
+    signIn('test@email.com', '12345678');
   };
 
-  const logOutTest = async () => {
-    const res = await logOut();
-    console.log(res);
+  const logOutTest = () => {
+    logOut();
   };
 
   return (
@@ -32,20 +29,13 @@ export default function Home() {
       </Head>
       <main className="min-h-full">
         <h2 className="text-3xl">Welcome</h2>
-        <div className="flex gap-4">
-          <Link href="/sign-in">{t('sign-in')}</Link>
-          <Link href="/sign-up">Sign up</Link>
-          <Link href="/editor">Editor</Link>
-        </div>
-
-        <div className="flex flex-col gap-2">
-          <button onClick={signUpTest}>Sign Up</button>
+        <div className="flex flex-col items-center gap-2">
+          <button onClick={signUpTest}>{t('sign-up')}</button>
           {isAuth ? (
-            <button onClick={logOutTest}>Log out</button>
+            <button onClick={logOutTest}>{t('log-out')}</button>
           ) : (
-            <button onClick={signInTest}>Sign In</button>
+            <button onClick={signInTest}>{t('sign-in')}</button>
           )}
-
           <div>{userEmail}</div>
         </div>
       </main>
