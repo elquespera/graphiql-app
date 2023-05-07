68c9fb998bb1dcda2426b33ec7d0ce13367918be
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-05-07 17:10:57 +0200

    feat: add translations for sign in and up pages

--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -1,28 +1,31 @@
 import AuthForm from '@/components/AuthForm';
+import useTranslation from '@/hooks/useTranslation';
 import Head from 'next/head';
 import Link from 'next/link';
 
 export default function SignInPage() {
+  const t = useTranslation();
+
   return (
     <>
       <Head>
-        <title>GraphQL - Sign in</title>
+        <title>{`${t('app-name')} - ${t('sign-in')}`}</title>
       </Head>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-white text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in
+          <h2 className="mt-10 text-center text-white text-2xl font-bold leading-9 tracking-tight">
+            {t('sign-in')}
           </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <AuthForm type="sign-in" />
           <p className="mt-10 text-center text-sm text-gray-500">
-            Do not have an account?{' '}
+            {`${t('do-not-have-account')} `}
             <Link
               href="/sign-up"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
-              Sign Up
+              {t('sign-up')}
             </Link>
           </p>
         </div>
