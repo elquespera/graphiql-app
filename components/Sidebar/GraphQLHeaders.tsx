e8d9b6c99d142204bbb9fc11c74382fde36e0245
Tomas Zagorskis <tomzagos@gmail.com>
2023-05-09 17:50:44 +0300

    feat: add headers component

---
+++ components/Sidebar/GraphQLHeaders.tsx
@@ -0,0 +1,5 @@
+export default function GraphQLHeaders() {
+  return (
+    <textarea className="border-gray-700 focus:border-gray-700 w-full h-full bg-inherit text-inherit resize-none focus:ring-4 focus:ring-gray-800" />
+  );
+}
