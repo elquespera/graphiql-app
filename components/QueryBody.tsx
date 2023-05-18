b7688350980889a0e95d7205f8cf045058406d0a
Sadullo <sadullo@gmail.com>
2023-05-18 07:16:13 +0500

    feat: add codemirror to query body

--- components/QueryBody.tsx
+++ components/QueryBody.tsx
@@ -2,26 +2,30 @@ import useTranslation from '@/hooks/useTranslation';
 import { selectGraphQlQuery, setQueryBody } from '@/redux/graphQlQuery';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { codemirrorTheme, basicSetup } from '@/constants/codemiror';
 
 export default function QueryBody() {
   const t = useTranslation();
   const dispatch = useAppDispatch();
   const { query } = useAppSelector(selectGraphQlQuery);
 
-  const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-    dispatch(setQueryBody(event.target.value));
+  const handleChange = (value: string) => {
+    dispatch(setQueryBody(value));
   };
 
   return (
     <div className="bg-slate-950 flex-[2] p-4 flex flex-col basis-full">
       <h2>{t('query')}</h2>
-      <div className="flex-1 flex flex-col mt-2">
-        <textarea
-          className="flex-1 bg-inherit text-inherit border-gray-700 focus:border-gray-700 resize-none focus:ring-4 focus:ring-gray-800"
-          value={query}
-          onChange={handleChange}
-        />
-      </div>
+      <CodeMirror
+        className="flex-1 flex flex-col mt-2"
+        value={query}
+        onChange={handleChange}
+        extensions={[javascript()]}
+        theme={codemirrorTheme}
+        basicSetup={basicSetup}
+      />
     </div>
   );
 }
