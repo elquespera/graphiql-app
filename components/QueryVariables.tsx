3963cc8a04fd0b7534af2e3a43c91b0207f3cfcb
Sadullo <sadullo@gmail.com>
2023-05-18 09:10:55 +0500

    feat: implement codemirror for variables

--- components/QueryVariables.tsx
+++ components/QueryVariables.tsx
@@ -1,19 +1,25 @@
 import { selectGraphQlQuery, setQueryVariables } from '@/redux/graphQlQuery';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { codemirrorTheme, basicSetup } from '@/constants/codemiror';
 
 export default function QueryVariables() {
   const dispatch = useAppDispatch();
   const { variables } = useAppSelector(selectGraphQlQuery);
 
-  const handleChange: React.ChangeEventHandler<HTMLTextAreaElement> = (event) => {
-    dispatch(setQueryVariables(event.target.value));
+  const handleChange = (value: string) => {
+    dispatch(setQueryVariables(value));
   };
 
   return (
-    <textarea
-      className="border-slate-700 focus:border-slate-700 border-t-0 w-full h-full bg-inherit text-inherit resize-none focus:ring-4 focus:ring-slate-800"
+    <CodeMirror
+      className="w-full h-full cm-variables"
       value={variables}
       onChange={handleChange}
+      extensions={[javascript()]}
+      theme={codemirrorTheme}
+      basicSetup={basicSetup}
     />
   );
 }
