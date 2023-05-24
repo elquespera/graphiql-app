4c79948456b7506e1861120883038945a33fac80
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-05-24 07:48:13 +0200

    fix: better example queries formatting

--- constants/apiUrls.ts
+++ constants/apiUrls.ts
@@ -5,96 +5,99 @@ export const API_URLS: ApiUrlInfo[] = [
     id: 1,
     url: 'https://graphqlpokemon.favware.tech',
     exampleQuery: `fragment data on Pokemon {
-    num
-    species
-    types
-    abilities { first second hidden }
-    baseStats { hp attack defense specialattack specialdefense speed }
-    gender { male female }
-    height
-    weight
-    flavorTexts { game flavor }
-    evYields { hp attack defense specialattack specialdefense speed }
-    isEggObtainable
-    minimumHatchTime
-    maximumHatchTime
-    levellingRate
-    sprite
-    shinySprite
-    backSprite
-    shinyBackSprite
-    smogonTier
-    smogonPage
-    serebiiPage
-    bulbapediaPage
-  }
+  num
+  species
+  types
+  abilities { first second hidden }
+  baseStats { hp attack defense specialattack specialdefense speed }
+  gender { male female }
+  height
+  weight
+  flavorTexts { game flavor }
+  evYields { hp attack defense specialattack specialdefense speed }
+  isEggObtainable
+  minimumHatchTime
+  maximumHatchTime
+  levellingRate
+  sprite
+  shinySprite
+  backSprite
+  shinyBackSprite
+  smogonTier
+  smogonPage
+  serebiiPage
+  bulbapediaPage
+}
   
   query($pokemon: PokemonEnum! $reverse: Boolean! $take: Int!) {
   getPokemon(pokemon: $pokemon reverseFlavorTexts: $reverse takeFlavorTexts: $take) {
     ...data
   }
-  }`,
+}`,
     exampleVariables: `{
-    "pokemon": "arceus",
-    "reverse": true,
-    "take": 1
-   }`,
+  "pokemon": "arceus",
+  "reverse": true,
+  "take": 1
+}`,
   },
   {
     id: 2,
     url: 'https://spacex-production.up.railway.app',
     exampleQuery: `{
-      launchesPast(limit: 10) {
-        mission_name
-        launch_date_local
-        launch_site {
-          site_name_long
-        }
-        links {
-          article_link
-          video_link
-        }
-        rocket {
-          rocket_name
-        }
-      }
-    }`,
+  launchesPast(limit: 10) {
+    mission_name
+    launch_date_local
+    launch_site {
+      site_name_long
+    }
+
+    links {
+      article_link
+      video_link
+    }
+
+    rocket {
+      rocket_name
+    }
+  }
+}`,
   },
   {
     id: 3,
     url: 'https://swapi-graphql.netlify.app/.netlify/functions/index',
     exampleQuery: `query Query {
-    allFilms {
-      films {
-        title
-        director
-        releaseDate
-        speciesConnection {
-          species {
+  allFilms {
+    films {
+      title
+      director
+      releaseDate
+      speciesConnection {
+        species {
+          name
+          classification
+          homeworld {
             name
-            classification
-            homeworld {
-              name
-            }
           }
         }
       }
     }
-  }`,
+  }
+}`,
   },
   {
     id: 4,
     url: 'https://rickandmortyapi.com/graphql',
     exampleQuery: `{
-    characters(page: 2, filter: { name: "rick" }) {
-      info {
-        count
-      }
-      results {
-        name
-      }
+  characters(page: 2, filter: { name: "rick" }) {
+    info {
+      count
     }
-  }`,
+
+    results {
+      name
+    }
+  }
+}`,
   },
   {
     id: 5,
@@ -111,17 +114,17 @@ export const API_URLS: ApiUrlInfo[] = [
     id: 6,
     url: 'https://graphql.anilist.co',
     exampleQuery: `{
-    Page {
-      media {
-        siteUrl
-        title {
-          english
-          native
-        }
-        description
+  Page {
+    media {
+      siteUrl
+      title {
+        english
+        native
       }
+      description
     }
-  }`,
+  }
+}`,
   },
 ];
 
