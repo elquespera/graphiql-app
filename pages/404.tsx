9a3b7c943788619105b674f6a98934ff9da313f3
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2023-05-27 10:53:29 +0200

    fix: load translations only on client to prevent ssr mismatch

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,16 +1,23 @@
 import useTranslation from '@/hooks/useTranslation';
 import Button from '@/components/Button';
+import { useEffect, useState } from 'react';
 
-const NotFound: React.FC = () => {
+export default function NotFound() {
   const t = useTranslation();
+  const [notFound, setNotFound] = useState('');
+  const [goToMain, setGoToMain] = useState('');
+
+  useEffect(() => {
+    setNotFound(t('not-found'));
+    setGoToMain(t('go-to-main'));
+  }, []);
+
   return (
-    <div className="flex flex-col items-center h-screen justify-center gap-16">
-      <h2 className="md:text-7xl text-4xl text-center">404: {t('not-found')}</h2>
+    <div className="flex flex-col items-center h-screen justify-center gap-16 p-4 md:p-8">
+      <h2 className="md:text-6xl text-4xl text-center">404: {notFound}</h2>
       <Button href="/" className="whitespace-nowrap">
-        {t('go-to-main')}
+        {goToMain}
       </Button>
     </div>
   );
-};
-
-export default NotFound;
+}
